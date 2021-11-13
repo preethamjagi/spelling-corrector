3f3381f34efc8f92a1936c9e571119427a9962dc
PREETHAM M <88784820+preethamjagi@users.noreply.github.com>
2021-11-13 13:15:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,63 @@
-import textgears from 'textgears-api';
-let input = document.querySelector('input')
-let textarea = document.querySelector('textarea')
+var d = "https://sg.api.textgears.com/spelling?"
+var key = "key=aiAu8Y3OaN0Pokab&text="
+var t = "&language=en-GB"
+var more;
+var inter = count();
 
-input.addEventListener('change', () => {
-	let files = input.files;
+function putit(i, j){
+    var text = more.response.errors[i].better[j];
+    var err = more.response.errors[i].bad;
+    var mod = '\\b'+err+'\\b';
+	var p = document.getElementById("demo").innerHTML;
+    p = p.replace(new RegExp(mod, "i"), 
+    		'<div style="display: inline-block; background-color: whitesmoke;">'+text+'</div>')
+	document.getElementById("demo").innerHTML = p;
+    var e = document.getElementById("memo");
+    var first = e.firstElementChild;
+    while (first) {
+        first.remove();
+        first = e.firstElementChild;
+    }
+}
 
-	if (files.length == 0) return;
+function listing(i){
+	var lnth = more.response.errors[i].better.length;
+    for (let j = 0; j < lnth ; j++){
+    	var text = more.response.errors[i].better[j];
+    	var node = document.createElement("li");
+  		var textnode = document.createTextNode(text);
+        var att = document.createAttribute("onclick");
+        var att2 = document.createAttribute("style");
+        att.value = "putit("+i+","+j+")";
+        att2.value = "cursor: pointer";
+  		node.appendChild(textnode);
+        node.setAttributeNode(att);
+        node.setAttributeNode(att2);
+  		document.getElementById("memo").appendChild(node);
+    }
+}
 
-	const file = files[0];
-
-	let reader = new FileReader();
-
-	reader.onload = (e) => {
-		const file = e.target.result;
-		const lines = file.split(/\r\n|\n/);
-		textarea.value = lines.join('\n');
-	}
-	
-
-	
-
-	reader.onerror = (e) => alert(e.target.error.name);
-	reader.readAsText(file);
-
-
-	const textgearsApi = textgears('JjfNDjpKeGfDmpnF', {language: 'en-US'});
-	textgearsApi.checkGrammar('An essay is nothing but a piece of content which is written from the perception of writer or author. Essays are similar to a story, pamphlet, thesis, etc. The best thing about Essay is you can use any type of language – formal or informal.')
-    .then((data) => {
-        for (const error of data.errors) {
-            console.log('Error: %s. Suggestions: %s', error.bad, error.better.join(', '));
+function count(){
+    return function(data){
+    var res = document.getElementById("finp").value;
+    var along = data.response.errors;
+    for (let i = 0; i < along.length ; i++) {
+    	var bad = along[i].bad;
+        var bab = '\\b'+bad+'\\b';
+    	res = res.replace(new RegExp(bab, "i"), '<div style="display: inline-block; cursor: pointer; background-color: #fdfd96;" onclick="listing('+i+')">'+bad+'</div>')
         }
-    })
-    .catch((err) => {});
-});
+    return res;
+    }
+}
+
+function loadDoc() {
+  const xhttp = new XMLHttpRequest();
+  var res = document.getElementById("finp").value.split(' ').join('+');
+  var url = d+key+res+t;
+  xhttp.onload = function() {
+  	more = JSON.parse(this.responseText);
+    document.getElementById("demo").innerHTML = inter(more);
+  }
+  xhttp.open("GET", url);
+  xhttp.send();
+}
